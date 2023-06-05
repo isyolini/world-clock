0a3cb4ee6c4b9612a76a04da6c9b49a501c2169f
isyolini <50636149+isyolini@users.noreply.github.com>
2023-06-05 19:02:24 +0200

    Fixed Selector time function

--- js/app.js
+++ js/app.js
@@ -44,15 +44,21 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace(" ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>`;
+
+  setTimeout(() => {
+    updateCity(event);
+  }, 1000);
 }
 
 updateTime();
